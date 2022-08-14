34b4dca2adaa47ee19a58dd5a8c54bfe5e81e023
Rishikesh Pravin Yelne <rishikeshyelne95@gmail.com>
2022-08-14 04:48:47 -0400

    BinaryTree

---
+++ LeetCodeTrials/MirrorOfBinaryTree.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCodeTrials
+{
+    internal class MirrorOfBinaryTree
+    {
+        public MirrorOfBinaryTree()
+        {
+            TreeNode t1 = new TreeNode(4, null, null);
+            TreeNode t2 = new TreeNode(5, null, null);
+            TreeNode t3 = new TreeNode(6, null, null);
+            TreeNode t4 = new TreeNode(7, null, null);
+            TreeNode t5 = new TreeNode(2, t1, t2);
+            TreeNode t6 = new TreeNode(3, t3, t4);
+            TreeNode t7 = new TreeNode(1, t5, t6);
+
+            Mirror(t7);
+        }
+
+        public void Mirror(TreeNode root)
+        {
+            if (root == null) return;
+
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                TreeNode node = queue.Dequeue();
+                TreeNode tempNode = node.left;
+                node.left = node.right;
+                node.right = tempNode;
+
+                if (node.left != null) queue.Enqueue(node.left);
+                if (node.right != null) queue.Enqueue(node.right);
+            }
+        }
+    }
+}
