944dcd65885f39c1721ef8e9eddec2868582ace3
Rishikesh Pravin Yelne <rishikeshyelne95@gmail.com>
2022-08-15 00:44:14 -0400

    BST

---
+++ LeetCodeTrials/ConnectNextPointerBT.cs
@@ -0,0 +1,69 @@
+﻿namespace LeetCodeTrials
+{
+    public class BinaryTreeNode
+    {
+        public int val;
+        public BinaryTreeNode left;
+        public BinaryTreeNode right;
+        public BinaryTreeNode next;
+
+        public BinaryTreeNode() { }
+
+        public BinaryTreeNode(int _val)
+        {
+            val = _val;
+        }
+
+        public BinaryTreeNode(int _val, BinaryTreeNode _left, BinaryTreeNode _right, BinaryTreeNode _next)
+        {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    internal class ConnectNextPointerBT
+    {
+        public ConnectNextPointerBT()
+        {
+            BinaryTreeNode t1 = new BinaryTreeNode(4, null, null, null);
+            BinaryTreeNode t2 = new BinaryTreeNode(5, null, null, null);
+            BinaryTreeNode t3 = new BinaryTreeNode(6, null, null, null);
+            BinaryTreeNode t4 = new BinaryTreeNode(7, null, null, null);
+            BinaryTreeNode t5 = new BinaryTreeNode(2, t1, t2, null);
+            BinaryTreeNode t6 = new BinaryTreeNode(3, t3, t4, null);
+            BinaryTreeNode t7 = new BinaryTreeNode(1, t5, t6, null);
+
+            var result = Connect(t7);
+            Console.WriteLine($"Result : {result}");
+        }
+
+        public BinaryTreeNode Connect(BinaryTreeNode root)
+        {
+            if (root == null) return null;
+            Queue<BinaryTreeNode> queue = new Queue<BinaryTreeNode>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                int size = queue.Count;
+                BinaryTreeNode initNode = queue.Dequeue();
+                if (initNode.left != null) queue.Enqueue(initNode.left);
+                if (initNode.right != null) queue.Enqueue(initNode.right);
+
+                for (int i = 1; i < size; i++)
+                {
+                    BinaryTreeNode node = queue.Dequeue();
+                    initNode.next = node;
+
+                    if (node.left != null) queue.Enqueue(node.left);
+                    if (node.right != null) queue.Enqueue(node.right);
+                    initNode = node;
+                }
+                initNode.next = null;
+            }
+            return root;
+        }
+    }
+}
---
+++ LeetCodeTrials/SearchBST.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCodeTrials
+{
+    internal class SearchBST
+    {
+        public SearchBST()
+        {
+            TreeNode t1 = new TreeNode(4, null, null);
+            TreeNode t2 = new TreeNode(5, null, null);
+            TreeNode t3 = new TreeNode(6, null, null);
+            TreeNode t4 = new TreeNode(7, null, null);
+            TreeNode t5 = new TreeNode(2, t1, t2);
+            TreeNode t6 = new TreeNode(3, t3, t4);
+            TreeNode t7 = new TreeNode(1, t5, t6);
+
+            var result = SearchInBST(t7, 4);
+            Console.WriteLine($"Result : {result}");
+        }
+
+        public TreeNode SearchInBST(TreeNode root, int val)
+        {
+            while (root != null)
+            {
+                if (root.val == val) return root;
+                if (root.val > val) root = root.left;
+                else root = root.right;
+            }
+            return null;
+        }
+    }
+}
