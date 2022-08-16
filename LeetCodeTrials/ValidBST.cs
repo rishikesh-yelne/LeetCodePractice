349f06f16e940ce826341b08ff1aa883df0a9538
Rishikesh Pravin Yelne <rishikeshyelne95@gmail.com>
2022-08-16 13:55:12 -0400

    BST

--- LeetCodeTrials/ValidBST.cs
+++ LeetCodeTrials/ValidBST.cs
@@ -2,25 +2,33 @@
 {
     internal class ValidBST
     {
+        public ValidBST()
+        {
+            TreeNode t1 = new TreeNode(3);
+            TreeNode t2 = new TreeNode(8);
+            TreeNode t3 = new TreeNode(4);
+            TreeNode t4 = new TreeNode(7, t1, t2);
+            TreeNode t5 = new TreeNode(5, t3, t4);
+
+            var result = IsValidBST(t5);
+            Console.WriteLine($"Result : {result}");
+        }
         public bool IsValidBST(TreeNode root)
         {
-            return CheckIfValidBST(root);
+            if (root == null) return true;
+            return CheckIfValidBST(root, long.MinValue, long.MaxValue);
         }
 
-        private bool CheckIfValidBST(TreeNode node)
+        private bool CheckIfValidBST(TreeNode node, long lowerBound, long upperBound)
         {
-            if (node == null) return true;
-            if (node.left != null)
+            if (node.val > lowerBound && node.val < upperBound)
             {
-                if (node.left.val > node.val) return false;
-                return CheckIfValidBST(node.left);
+                bool leftTree = true, rightTree = true;
+                if (node.left != null) leftTree = CheckIfValidBST(node.left, lowerBound, node.val);
+                if (node.right != null) rightTree = CheckIfValidBST(node.right, node.val, upperBound);
+                return leftTree && rightTree;
             }
-            if (node.right != null)
-            {
-                if (node.right.val < node.val) return false;
-                return CheckIfValidBST(node.right);
-            }
-            return true;
+            else return false;
         }
     }
 }
