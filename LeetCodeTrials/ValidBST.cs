603f4754ad9b7b5a80e833637e5d03bddc3f6343
Rishikesh Pravin Yelne <rishikeshyelne95@gmail.com>
2022-08-15 23:58:13 -0400

    BST

---
+++ LeetCodeTrials/ValidBST.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeTrials
+{
+    internal class ValidBST
+    {
+        public bool IsValidBST(TreeNode root)
+        {
+            return CheckIfValidBST(root);
+        }
+
+        private bool CheckIfValidBST(TreeNode node)
+        {
+            if (node == null) return true;
+            if (node.left != null)
+            {
+                if (node.left.val > node.val) return false;
+                return CheckIfValidBST(node.left);
+            }
+            if (node.right != null)
+            {
+                if (node.right.val < node.val) return false;
+                return CheckIfValidBST(node.right);
+            }
+            return true;
+        }
+    }
+}
