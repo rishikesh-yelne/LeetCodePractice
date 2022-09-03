8153579c3924909fda4c0de4d7b31ced82c4ad48
Rishikesh Pravin Yelne <ryelne@ncsu.edu>
2022-09-03 01:12:51 -0400

    Greedy

---
+++ LeetCodeTrials/MinHealthToBeatGame.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCodeTrials
+{
+    internal class MinHealthToBeatGame
+    {
+        public MinHealthToBeatGame()
+        {
+            var result = MinimumHealth(new int[] { 2, 7, 4, 3 }, 4);
+            Console.WriteLine($"Result : {result}");
+        }
+
+        public long MinimumHealth(int[] damage, int armor)
+        {
+            long health = 0, maxDamage = 0;
+            for (int i = 0; i < damage.Length; i++)
+            {
+                maxDamage = Math.Max(maxDamage, (long)damage[i]);
+                health += (long)damage[i];
+            }
+            return health + 1 - Math.Min(armor, maxDamage);
+        }
+    }
+}
