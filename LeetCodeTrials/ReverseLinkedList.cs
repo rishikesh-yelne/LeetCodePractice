c7f671327ff0b486420f56e25ab881bfc42cb667
Rishikesh Pravin Yelne <rishikeshyelne95@gmail.com>
2023-06-17 21:47:38 -0700

    Linked List

---
+++ LeetCodeTrials/ReverseLinkedList.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCodeTrials
+{
+    internal class ReverseLinkedList
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode curr = head;
+            ListNode prev = null;
+
+            while (curr != null)
+            {
+                ListNode temp = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = temp;
+            }
+
+            return prev;
+        }
+    }
+}
